da4dc582f39aaf7e6571f2d69e4ff937b39c1c58
Ignacio <morinacho@gmail.com>
2018-10-22 13:24:54 -0300

    Modifique la conexion para trabajar con la base de datos reducida

--- models/ConexionModel.php
+++ models/ConexionModel.php
@@ -3,7 +3,12 @@
 	class ConexionModel{
 
 		public function conect(){
-			$link = new PDO("mysql:host=localhost;dbname=me-library", "root", "");
+			/*
+			 * La base de datos ha sido reducida para llegar al trabajo de base de datos
+			 * Cambiar a la base de datos completa llamada me-library
+			 */
+			
+			$link = new PDO("mysql:host=localhost;dbname=me-library-reduce", "root", "");
 			return $link;
 		}
 	}
