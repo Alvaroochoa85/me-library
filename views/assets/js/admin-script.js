9225a980bf014cfc86c0acfed549678f62a30908
Ignacio <morinacho@gmail.com>
2018-10-22 20:49:41 -0300

    Funcionalidad para solo aceptar numeros input type number. Funcionalidad de copia dni en password

--- views/assets/js/admin-script.js
+++ views/assets/js/admin-script.js
@@ -15,6 +15,15 @@ $(document).ready(function(){
 		}
 		
 	});
+	
+	//Function which allows only the entry numbers
+	$('.justNumbers').keypress(function(e){
+		var keynum = window.event ? window.event.keyCode : e.which;
+   		if ((keynum == 48) || (keynum == 56))
+        	return true;
+    	return /\d/.test(String.fromCharCode(keynum));
+	});
+
 	//Control de imagenes
 	$("#user-img").change(function( ) {
 		image = this.files[0];
